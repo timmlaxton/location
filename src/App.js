0d691b71cb46a7f0ffac46406874b01fa931c8ce
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-08-03 13:42:01 +0100

    error

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import{ BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom
 
 import Users from './user/pages/Users'
 import NewPlace from './places/pages/NewPlace'
+import MainNavigation from './shared/components/Navigation/MainNavigation'
 
 
 function App() {
   return (
    <Router>
+     <MainNavigation/>
      <Switch> 
     <Route path="/" exact>
     <Users/>
---
+++ src/shared/components/Navigation/MainNavigation.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+import MainHeader from './MainHeader'
+import './MainNavigation.css'
+
+const MainNavigation = props => {
+    return  (
+    <MainHeader>
+        <button className="main-navigation_menu-btn">
+            <span></span>
+            <span></span>
+            <span></span>
+        </button>
+        <h1 className="main-navigation_title">
+        <Link to="/"> Your Places </Link>
+        </h1>
+        <nav>
+        ...
+        </nav>
+    </MainHeader>
+    )}
+
+export default MainNavigation;
