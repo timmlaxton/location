280f6f416e4472888ed93e2a14855daee6f99897
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-08-03 13:00:56 +0100

    added styling to img

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -1,20 +1,26 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 
-
+import Avatar from '../../shared/components/UIElements/Avatar'
+import Card from '../../shared/components/UIElements/Card'
 import './UserItem.css'
 
 const UsersItem = props => {
     return (
         <li className='user-item'>
-            <div className='user-item_content'>
+            
+                <Card className='user-item_content'>  
+                <Link to={`/${props.id}/places`}> 
             <div className='user-item_image'>
-            <img src={props.image} alt={props.name}/>
+            <Avatar image={props.image} alt={props.name} />
             </div>
             <div className="user-item__info">
             <h2>{props.name}</h2>
             <h3>{props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}</h3>
             </div>
-            </div>
+            </Link>
+            </Card>
+            
         </li>
     );
 
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -6,8 +6,8 @@ const Users = () => {
     const USER = [
         {
             id: 'u1', 
-            name: 'tim laxton', 
-            image: 'https://images.pexels.com/photos/1161547/pexels-photo-1161547.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', 
+            name: 'Tim Laxton', 
+            image: 'https://images.pexels.com/photos/57627/pexels-photo-57627.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500', 
             places: 2 
         }
     ];
